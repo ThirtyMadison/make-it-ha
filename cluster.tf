519cf0f9bf1399dd785254dcf5abec7913918744
Lawrence Wakefield <lawrence@thirtymadison.com>
2021-07-22 07:25:39 -0400

    Remove iam user map

--- cluster.tf
+++ cluster.tf
@@ -15,24 +15,6 @@ module "eks_cluster" {
   region     = "us-east-2"
   vpc_id     = data.terraform_remote_state.base.outputs.vpc_id
   subnet_ids = data.terraform_remote_state.base.outputs.subnet_ids
-  map_additional_iam_users = [
-    # arn:aws:iam::033364887666:user/candidate1
-    # list(object({
-    #   rolearn  = string
-    #   username = string
-    #   groups   = list(string)
-    # }))
-    {
-      userarn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/candidate${local.identifier}"
-      username = "candidate${local.identifier}"
-      groups   = ["system:masters"]
-    },
-    {
-      userarn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/interviewer${local.identifier}"
-      username = "interviewer${local.identifier}"
-      groups   = [ "system:masters" ]
-    }
-  ]
 
   kubernetes_version    = local.kubernetes_version
   workers_role_arns     = [module.eks_node_group.eks_node_group_role_arn]
